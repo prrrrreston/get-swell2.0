20ea30e1f2a4d0389d372df98efcf403b05d613d
Siddhant Saxena <sidsaxena27@gmail.com>
2023-09-05 19:52:24 -0500

    Added login page

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-
+// Appending app component to root
+// add react router here
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
