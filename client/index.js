b1166761e4bfc36d657b8937bf21f606a850cbb7
aydenyipcs <aydenyip56@gmail.com>
2023-09-07 18:17:37 -0400

    Co-authored-by: Justin Chu <jchu47@users.noreply.github.com> Co-authored-by: sidsaxena27 <sidsaxena27@users.noreply.github.com> Co-authored-by: prrrrreston <prrrrreston@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Routes, useNavigate,
+} from 'react-router-dom';
+import { Construction } from '@mui/icons-material';
 import App from './App';
 import LoginSignup from './components/Login-Signup';
 // Appending app component to root
@@ -8,13 +11,43 @@ import LoginSignup from './components/Login-Signup';
 // const root = createRoot(document.getElementById('root'));
 // root.render(<App />);
 
-const root = createRoot(document.getElementById('root'));
+const authenticate = () => {
+  const [isAuth, setIsAuth] = useState(null);
 
-root.render(
-  <BrowserRouter>
+  useEffect(() => {
+    fetch('/verify')
+      .then((res) => res.json())
+      .then((data) => {
+        setIsAuth(data.isAuthenticated);
+      });
+  }, []);
+  return isAuth;
+};
+
+function RouterAuth() {
+  const navigate = useNavigate();
+  const isAuthenticated = authenticate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/app');
+    }
+  }, [isAuthenticated]);
+  if (isAuthenticated === null || isAuthenticated === false) {
+    navigate('/');
+  }
+  return (
     <Routes>
       <Route path="/" element={<LoginSignup />} />
       <Route path="/app" element={<App />} />
     </Routes>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <RouterAuth />
   </BrowserRouter>,
 );
