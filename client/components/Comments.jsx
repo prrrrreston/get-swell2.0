20c8fa3a3a6445e537e7add10fb0f1a6e311862b
Siddhant Saxena <sidsaxena27@gmail.com>
2023-09-07 11:06:48 -0500

    Added comments to posts and submit comment functionality

--- client/components/Comments.jsx
+++ client/components/Comments.jsx
@@ -3,6 +3,10 @@ import '../stylesheets/Post.css';
 import octopus from '../assets/octopus-tentacles.png';
 
 function Comment(props) {
+
+  
+
+
   return (
     <div className="post">
       <b>{props.username}</b>
--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -3,12 +3,18 @@ import Cookies from 'js-cookie';
 import '../stylesheets/Post.css';
 import octopus from '../assets/octopus-tentacles.png';
 import testImg from '../assets/testImg.js';
+import Comment from './Comments';
 
 function Post(props) {
   const { setFeedChange, postInfo } = props;
+  const [comment, setComment] = useState('')
   console.log('creating a post');
   console.log('postInfo:', postInfo);
 
+  const handleCommentChange = (e) => {
+    setComment(e.target.value)
+  }
+  
   const deletePost = (event) => {
     const postID = event.target.parentNode.id;
     fetch(`/api/posts/${postID}`, {
@@ -39,6 +45,29 @@ function Post(props) {
       .catch((error) => console.log(error));
   };
 
+  const submitComment = (event) => {
+    const postID = event.target.parentNode.parentNode.id;
+    const username = Cookies.get('username')
+    fetch(`/api/posts/comment/`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({postID, username, comment})
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setFeedChange(true);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const commentList = [];
+  
+  for (let i = 0; i < postInfo.comments.length; i++) {
+    commentList.push(<Comment key={i} username={postInfo.comments[i].username} comment={postInfo.comments[i].comment} />)
+  }
+
+
   return (
     <div className="post" id={props.postInfo._id}>
       <img src={props.postInfo.image || octopus} />
@@ -61,6 +90,16 @@ function Post(props) {
         <button onClick={likePost} style={{ marginLeft: '10px' }}>Like Post</button>
       </p>
       <button onClick={deletePost}>Delete Post</button>
+      <div className="comment-section">
+        <input 
+          type="text" 
+          placeholder="Add a comment..." 
+          value={comment}
+          onChange={handleCommentChange}
+        />
+        <button onClick={submitComment}>Comment</button>
+      </div>
+      {commentList}
     </div>
   );
 }
